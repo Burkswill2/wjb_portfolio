1f7782204e3e65493cc36a3b93721eddf97004fd
Will Burks <burks.113@osu.edu>
2024-03-07 14:11:26 -0500

    Added jsdoc

--- frontend_react/src/containers/Work/Work.jsx
+++ frontend_react/src/containers/Work/Work.jsx
@@ -1,9 +1,30 @@
 import * as React from 'react';
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 import './Work.scss'
 
 const Work = () => {
+
+    const handle
+
     return (
-        <div>Work</div>
+        <>
+            <h2 className="head-text"> My Creative <span>Portfolio</span> Section</h2>
+
+            <div className="app__work-filter">
+                {['UI/UX', 'Web App', 'Mobile App', 'ReactJS', 'All'].map((item, index) => (
+                    <div
+                        key={index}
+                        onClick={() => handleWorkFilter(item)}>
+                        {item}
+                    </div>
+                ))}
+            </div>
+
+        </>
     );
 }
 
